da30800b3b60920911428f06b5f6b9f7d91b0239
lhercas798 <leohc798@hotmail.com>
2022-12-06 19:43:56 +0100

    quinta

--- src/component/inventario.jsx
+++ src/component/inventario.jsx
@@ -7,7 +7,9 @@ const Inventario = ({ inicial, deseo, elimina }) => {
         return (
           <div key={index} className="card">
             <img src={`/img/${item.imagen}`} alt="" />
-            <span style={{ fontSize: "30px" }}>{item.whish ? "💝" : "🖤"}</span>
+            <span onClick={
+                  item.whish ? () => elimina(item, index) : () => deseo(index)
+                } style={{ fontSize: "30px",cursor:'pointer' }}>{item.whish ? "💝" : "🖤"}</span>
             <div className="container">
               <h1>🎁Felices Fiestas🎁</h1>
               <h1>{item.name}</h1>
